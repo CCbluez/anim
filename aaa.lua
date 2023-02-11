9b13a61730a70cd5a42ec813ded861d64f5c8432
N/ABreadsticks <91288666+CCbluez@users.noreply.github.com>
2023-02-11 14:40:21 -0800

    Create aaa.lua

---
+++ aaa.lua
@@ -0,0 +1,8 @@
+local Anim = Instance.new("Animation")
+Anim.AnimationId = "rbxassetid://4940561610"
+local confused = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
+confused.Priority = Enum.AnimationPriority.Action4;
+confused:Play()
+wait(1)
+
+confused:Stop()
